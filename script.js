346071d23665252b48553641226ced2778947027
Albert Nutifafa Dovlo <albert.dovlo@amalitech.org>
2023-01-31 05:36:27 +0000

    changes made

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const keys = calculator.querySelector('.keypad');
 
 keys.addEventListener('click', (e) => {
   if (!e.target.matches('button')) {
+    const key = e.target
+    Array.from(key.parentNode.children)
+    .forEach(k => k.classList.remove('is-depressed'))
     return;
   }
 
@@ -15,6 +18,7 @@ keys.addEventListener('click', (e) => {
   console.log(keyContent);
   const displayedNum = display.textContent;
   console.log(displayedNum);
+  const previousKeyType = calculator.dataset.previousKeyType
 
   if (
     action === 'add' ||
@@ -22,7 +26,19 @@ keys.addEventListener('click', (e) => {
     action === 'multiply' ||
     action === 'divide'
   ) {
-    console.log('operator key!')
+    // console.log('operator key!');
+    // key.classList.add('is-depressed')
+    // calculator.dataset.previousKeyType = 'operator'
+    calculator.dataset.firstValue = displayedNum
+    calculator.dataset.operator = action
+  }
+
+  if (!action) {
+    if (displayedNum === '0' || previousKeyType === 'operator') {
+      display.textContent = keyContent
+    } else {
+      display.textContent = displayedNum + keyContent
+    }
   }
 
   if (action === 'decimal') {
@@ -34,8 +50,13 @@ keys.addEventListener('click', (e) => {
   }
   
   if (action === 'calculate') {
-    console.log('calculate key!')
+    const firstValue = calculator.dataset.firstValue
+    const operator = calculator.dataset.operator
+    const secondValue = displayedNum
+    
+    display.textContent = calculate(firstValue, operator, secondValue)
   }
+
   if (action === 'delete') {
     console.log('delete key!')
   }
@@ -46,5 +67,24 @@ keys.addEventListener('click', (e) => {
     } else {
       display.textContent = displayedNum + keyContent;
     }
+  }else if(action === 'decimal') {
+    display.textContent = displayedNum + '.'
   }
 });
+
+
+const calculate = (n1, operator, n2) => {
+    let result = ''
+    
+    if (operator === 'add') {
+      result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+      result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+      result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+      result = parseFloat(n1) / parseFloat(n2)
+    }
+    
+    return result
+  }
