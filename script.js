86d5be787e7fae0101b6f353b1e2192cb6aa98e2
skwame49 <skwame49i@gmail.com>
2023-04-13 15:23:26 +0000

    update calculator function to include operators in display and to be able to calculate large numbers

--- script.js
+++ script.js
@@ -11,13 +11,10 @@ const header = document.querySelector(".header");
 const btnNum = document.querySelectorAll(".btn-Num");
 const btnDark = document.querySelectorAll(".btn-dark");
 const equals = document.querySelector(".equals");
-
 const toggle = document.querySelectorAll(".btn-theme");
-console.log(toggle);
 
 toggle.forEach((element) => {
   element.addEventListener("click", (e) => {
-    // console.log(e.target.id);
     if (e.target.id === "one") {
       (one.style.backgroundColor = "#D03F2F"),
       (two.style.backgroundColor = "transparent"),
@@ -43,7 +40,6 @@ toggle.forEach((element) => {
           btn.style.boxShadow = '0 4px #475172'
           btn.style.color = '#fff'
         })
-        // console.log("One");
       } else if (e.target.id == "two") {
         (one.style.backgroundColor = "transparent"),
         (two.style.backgroundColor = "#C85402"),
@@ -70,7 +66,7 @@ toggle.forEach((element) => {
           btn.style.color = '#fff'
         })
       
-        // body.classList.add("body-two");
+
       header.classList.add("header-two");
       keypad.classList.add("keypad-two");
       calculator.classList.add("calculator-two");
@@ -107,131 +103,115 @@ toggle.forEach((element) => {
           btn.style.boxShadow = '0 4px #9b4cc0'
           btn.style.color = '#fff'
         })
-        // const header = document.querySelector(".header");
-      // const keypad = document.querySelector(".keypad");
-      // const btnDark = document.querySelector(".btn-dark");
-      // const reset = document.querySelector(".reset");
-      // const equals = document.querySelector(".equals");
-      // const btnNum = document.querySelectorAll(".btn-Num");
-      // body.classList.add("body-three");
-      // header.classList.add("header-three");
-      // keypad.classList.add("keypad-three");
-      // calculator.classList.add("calculator-three");
-      // reset.classList.add("reset-three");
-      // btnNum.classList.add("btnNum-three");
-      // // display.classList.remove();
-      // display.style.backgroundColor = '#1E0936'
-      // display.style.color = '#FFE53D'
-      // // display.classList.add("display-three");
-      // toggleBtn.style.backgroundColor = '#1E0936'
-     
-      // display.classList.add("display-three");
-
-      // btnDark.style.backgroundColor = "#56077C";
-      // equals.style.backgroundColor = "#00DED0";
-      // equals.style.boxShadow = "0 4px #00DED0";
-      // btnDark.style.boxShadow = " 0 4px #8e1ec2";
-     
     }
   });
 });
 
-keys.addEventListener("click", (e) => {
-  // if (!e.target.matches('button')) {
-  //   const key = e.target
-  //   Array.from(key.parentNode.children)
-  //   .forEach(k => k.classList.remove('is-pressed'))
-  //   return;
-  // }
 
+keys.addEventListener("click", (e) => {
   const key = e.target;
-  // console.log(key);
   const action = key.dataset.action;
-  // console.log(action);
   const keyContent = key.textContent;
-  // console.log(keyContent);
   let displayedNum = display.textContent;
-  // console.log(displayedNum);
   const previousKeyType = calculator.dataset.previousKeyType;
-  // console.log(previousKeyType);
+
   if (
     action === "add" ||
     action === "subtract" ||
     action === "multiply" ||
     action === "divide"
   ) {
-    // console.log('operator key!');
-    // key.classList.add('is-pressed')
-    // calculator.dataset.previousKeyType = 'operator'
-    // displayedNum = keyContent
-    calculator.dataset.firstValue = displayedNum;
-    calculator.dataset.operator = action;
-    display.innerHTML = "";
-  }
-
-  if (!action) {
-    if (displayedNum === "0" || previousKeyType === "operator") {
-      display.textContent = keyContent;
+    if (previousKeyType === "operator") {
+      calculator.dataset.operator = action;
+    } else if (previousKeyType === "calculate") {
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     } else {
-      display.textContent = displayedNum + keyContent;
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      if (firstValue && operator && secondValue) {
+        displayedNum = calculate(firstValue, operator, secondValue);
+        display.textContent = displayedNum;
+      }
+
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
+
+    calculator.dataset.previousKeyType = "operator";
+    display.textContent = keyContent;
   }
 
-  if (action === "decimal") {
-    // displayedNum += ".";
-    console.log("decimal key!");
+  if (!action || action === "decimal") {
+    if (previousKeyType === "operator" || previousKeyType === "calculate") {
+      display.textContent = "";
+    }
+    display.textContent += keyContent;
+    calculator.dataset.previousKeyType = "number";
   }
 
   if (action === "clear") {
-    display.innerHTML = 0;
+    calculator.dataset.firstValue = "";
+    calculator.dataset.operator = "";
+    calculator.dataset.previousKeyType = "";
+    display.textContent = "";
   }
 
   if (action === "calculate") {
-    const firstValue = calculator.dataset.firstValue;
-    console.log(firstValue);
-    const operator = calculator.dataset.operator;
-    console.log(operator);
-    const secondValue = displayedNum;
-    console.log(secondValue);
+    if (previousKeyType !== "calculate") {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
 
-    display.textContent = calculate(firstValue, operator, secondValue);
+      if (firstValue && operator && secondValue) {
+        displayedNum = calculate(firstValue, operator, secondValue);
+        display.textContent = displayedNum;
+        calculator.dataset.firstValue = displayedNum;
+      }
+    }
+    calculator.dataset.previousKeyType = "calculate";
   }
 
   if (action === "delete") {
-    display.innerText = display.innerText.slice(0, -1);
-    console.log("delete key!");
-  }
-
-  if (!action) {
-    if (displayedNum === "0") {
-      display.textContent = keyContent;
-    } else {
-      display.textContent = displayedNum + keyContent;
-    }
-  } else if (action === "decimal") {
-    display.textContent = displayedNum + ".";
+    display.innerText = display.textContent.slice(0, -1);
+    calculator.dataset.previousKeyType = "operator";
   }
 });
+
+
+// Calculate numbers function
 
 const calculate = (n1, operator, n2) => {
   let result = "";
 
-  if (operator === "add") {
-    result = parseFloat(n1) + parseFloat(n2);
-  } else if (operator === "subtract") {
-    result = parseFloat(n1) - parseFloat(n2);
-  } else if (operator === "multiply") {
-    result = parseFloat(n1) * parseFloat(n2);
-  } else if (operator === "divide") {
-    result = parseFloat(n1) / parseFloat(n2);
+  const num1 = parseFloat(n1);
+  const num2 = parseFloat(n2);
+
+  // Check for valid operator
+  if (!isNaN(num1) && !isNaN(num2)) {
+    if (operator === "add") {
+      result = num1 + num2;
+    } else if (operator === "subtract") {
+      result = num1 - num2;
+    } else if (operator === "multiply") {
+      result = num1 * num2;
+    } else if (operator === "divide") {
+      if (num2 === 0) {
+        // Handle division by zero
+        result = "Error: Division by zero";
+      } else {
+        result = num1 / num2;
+      }
+    } else {
+      result = "Error: Invalid operator";
+    }
+  } else {
+    result = "Error: Invalid numbers";
   }
 
-  result = result.toString().split(".");
-  result[0] = result[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  result.join(".");
-  console.log("hello:::  " + result);
 
   return result;
 };
 
-////////////////////////////////////////////////////////////
